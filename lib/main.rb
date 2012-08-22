4b20a7dbe9ddf36aee66ea009f2286fddef5a61c
Pravin  Mishra <diatm.pravin.it.07.27@gmail.com>
2012-08-22 23:38:36 +0530

    first scraping

--- lib/main.rb
+++ lib/main.rb
@@ -1,4 +1,12 @@
 # To change this template, choose Tools | Templates
 # and open the template in the editor.
+require "rubygems"
+require "nokogiri"
+require "open-uri"
 
-puts "Hello World"
+url = "http://www.junglee.com/b/783367031?ie=UTF8&tag=googjuhydr-21&hvadid=11585076165&hvpos=1t1&hvexid=&hvnetw=g&hvrand=18525729661750602638&hvpone=&hvptwo=&hvqmt=b&ref=pd_sl_55xgssp2hz_b"
+doc = Nokogiri::HTML(open(url))
+doc.css(".asinItem").each do |each_product|
+  puts each_product.at_css(".title").text
+end
+
